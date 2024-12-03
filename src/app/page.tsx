a79f10737885095d225d72ce830cf53ac1fcc500
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-12-03 18:38:42 +0100

    Add theme switch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@nextui-org/react";
+import ThemeSwitch from "@/components/theme-switch";
 
 
 export default function Home() {
@@ -10,7 +10,7 @@ export default function Home() {
             Rodrigo<br></br>Guerreiro
           </span>
           Senior Full Stack Developer
-
+          <ThemeSwitch />
         </header>
 
       </div>
