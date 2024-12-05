8c64c752d2518fd570cee8034786ded93ea7f856
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-12-05 15:31:57 +0100

    Add experience list

--- src/components/experience-section.tsx
+++ src/components/experience-section.tsx
@@ -8,18 +8,23 @@ export default function ExperienceSection() {
       {
         jobs.map(job => {
           return (
-            <div className="flex gap-3 text-sm p-2 rounded-lg dark:bg-content1-800 bg-content1-200">
-              <div className="text-xs">2020 - 2024</div>
-              <div className="flex flex-col gap-2">
-                <div className="text-base">
+            <div className="flex gap-3 text-sm p-2 rounded-lg dark:bg-content1-800 bg-content1-200 items-center">
+              <div className="text-xs flex flex-col items-center">
+                <span>{job.start}</span>
+                <span className="whitespace-pre-line">{".\n.\n."}</span>
+                <span className="mt-1">{job.end}</span>
+                {job.endYear && <span className="mt-3">{job.endYear}</span>}
+              </div>
+              <div className="flex flex-col gap-3">
+                <div className="text-base text-primary">
                   {job.title}
                 </div>
                 <p className="">
                   {job.description}
                 </p>
-                <div className="flex gap-2">
+                <div className="flex gap-2 flex-wrap">
                   {
-                    job.stack.map(tech => <Chip variant="bordered" color="primary" size="sm">{tech}</Chip>)
+                    job.stack.map(tech => <Chip variant="flat" color="primary" size="sm">{tech}</Chip>)
                   }
                 </div>
               </div>
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,19 +1,75 @@
-type Job = {
-  startMonth: string
-  endMonth: string
-  startYear: number
-  endYear: number
+export type Job = {
+  start: string | number
+  end: string | number
+  endYear?: number
   title: string
   description: string
   stack: string[]
   company: string
   companyUrl: string
 }
+
 export const jobs: Job[] = [
   {
-    startMonth: "Feb",
-    endMonth: "Jul",
-    startYear: 2015,
+    start: 2021,
+    end: 2024,
+    title: "Senior Full Stack Developer",
+    description: "Full-Stack developer on an artist booking platform with in-app messaging,\
+      embedded media, ElasticSearch for quick artist search and filtering and A/B testing with Google Optimize. \
+      Also responsible for improving team/company processes, bringing Agile methodologies and best practices. \
+      Responsible for an API integration to consume gigmit data and perform authentication with JWT for a mobile app, \
+      in a project with 3 european partners",
+    stack: ["Ruby on Rails", "Backbone.js", "Postgres", "Docker", "SCRUM", "Elasticsearch", "Figma", "Kanban", "JWT"],
+    company: "realxdata (now a Moody's company)",
+    companyUrl: "https://www.moodyscre.com/persona/emea/"
+  },
+  {
+    start: 2019,
+    end: 2021,
+    title: "Full Stack Developer",
+    description: "Ruby on Rails and React developer on a real estate analytics platform, responsible for migrating \
+      the front-end to React, refactor and re-structure the backend and implement best practices, useful design patterns \
+      and impove test coverage, writing meaningful and reliable unit and integration tests.",
+    stack: ["Ruby on Rails", "React", "Postgres", "Docker", "SCRUM", "AWS"],
+    company: "realxdata (now a Moody's company)",
+    companyUrl: "https://www.moodyscre.com/persona/emea/"
+  },
+  {
+    start: 2017,
+    end: 2019,
+    title: "Full Stack Developer",
+    description: "Full-Stack developer, working mostly with Ruby on Rails and React and in a short project with \
+      Django and Android native development. The main projects I worked on were an e-commerce platform for agriculture \
+      goods and a fintech platform for alternative investments.",
+    stack: ["Ruby on Rails", "React", "Postgres", "SQLite", "Django", "Android"],
+    company: "Runtime Revolution",
+    companyUrl: "https://www.runtime-revolution.com/"
+  },
+  {
+    start: 2016,
+    end: 2017,
+    title: "Android Developer",
+    description: "Freelance project developing an Android app that receives data from multiple sensors in a truck \
+      to provide drivers with real time monitorization of core aspects of their load and route.",
+    stack: ["Java", "REST", "Android"],
+    company: "Apibeton",
+    companyUrl: "https://apibeton.com/"
+  },
+  {
+    start: 2015,
+    end: 2016,
+    title: "Junior Programmer",
+    description: "Part of the maintenance team on a project for a major insurance company in Portugal, working \
+      on their AS400 system (COBOL), reporting and fixing bugs. In a different internal project for Deloitte, I was \
+      responsible for the initial development of a website for HR management, designing the data model and developing \
+      the frontend layout.",
+    stack: ["HTML", "CSS", "Javascript", "COBOL"],
+    company: "Deloitte",
+    companyUrl: "https://www.deloitte.com/pt/pt.html"
+  },
+  {
+    start: "Feb",
+    end: "Jul",
     endYear: 2015,
     title: "Android Developer",
     description: "Internship as part of the final semester of my degree, where I was responsible \
