992d53c5ad2cbd6faae545367295e2acaf545ffb
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-12-06 10:38:16 +0100

    Add full cv file

--- src/components/experience-section.tsx
+++ src/components/experience-section.tsx
@@ -1,8 +1,8 @@
-import { Chip } from "@nextui-org/react";
+import { Link } from "@nextui-org/react";
 import Section from "./section";
 import { jobs } from "@/constants";
-import Link from "next/link";
 import Project from "./project";
+import Image from "next/image";
 
 export default function ExperienceSection() {
   return (
@@ -10,6 +10,16 @@ export default function ExperienceSection() {
       {
         jobs.map(job => (<Project job={job} />))
       }
+      <Link target="_blank" href="/cv.pdf" underline="always" className="text-sm">
+        View full CV
+        <Image
+          src="/file.svg"
+          width={15}
+          height={15}
+          alt="Resume"
+          className="ml-2"
+        />
+      </Link>
     </Section>
   )
 }
