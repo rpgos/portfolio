310e7ad355c10af28725a39b4a18734575c1cda9
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-12-09 11:07:15 +0100

    Add send email server action

---
+++ src/actions/send-email.ts
@@ -0,0 +1,59 @@
+import nodemailer from 'nodemailer'
+import Mail from 'nodemailer/lib/mailer';
+import { z } from "zod"
+
+const messageSchema = z.object({
+  name: z.string().min(3),
+  email: z.string().email(),
+  message: z.string().min(5),
+})
+
+interface MessageFormState {
+  errors: {
+    name?: string[],
+    email?: string[],
+    message?: string[],
+    _form?: string[]
+  }
+}
+
+export default async function sendEmail(formState: MessageFormState, formData: FormData): Promise<MessageFormState> {
+  const parsedMessage = messageSchema.safeParse({
+    name: formData.get('name'),
+    email: formData.get('email'),
+    message: formData.get('message'),
+  })
+
+  if(!parsedMessage.success) {
+    return {
+      errors: parsedMessage.error.flatten().fieldErrors
+    }
+  }
+
+  const transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.GMAIL_ID,
+      pass: process.env.GMAIL_SECRET,
+    }
+  })
+
+  const mailOptions: Mail.Options = {
+    from: process.env.GMAIL_ID,
+    to: process.env.GMAIL_ID,
+    subject: `Message from ${parsedMessage.data.name} - ${parsedMessage.data.email}`,
+    text: parsedMessage.data.message
+  }
+
+  const res = await transport.sendMail(mailOptions)
+  
+  if(res.accepted.length) {
+    return { errors: {} }
+  }
+  
+  return {
+    errors: {
+      _form: ['Failed to send message. Please try again later.']
+    }
+  }
+}
