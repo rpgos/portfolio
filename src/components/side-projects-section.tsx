e3e4337d1de18f0ed3f99d89bb8d8a0bb2e283fe
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-12-09 16:53:54 +0100

    Linting

--- src/components/side-projects-section.tsx
+++ src/components/side-projects-section.tsx
@@ -1,14 +1,12 @@
 import { projects } from "@/constants";
 import Section from "./section";
-import Link from "next/link";
-import { Chip } from "@nextui-org/react";
 import Project from "./project";
 
 export default function SideProjectsSection() {
   return (
     <Section id="projects" title="Side Projects">
       {
-        projects.map(project => (<Project job={project} />))
+        projects.map(project => (<Project key={project.company} job={project} />))
       }
     </Section>
   )
