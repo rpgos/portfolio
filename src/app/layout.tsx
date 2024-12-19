e6e786f4fc50dc0bf1e9a3afb9a7343a2b5d381d
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-12-19 20:47:09 +0000

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Providers from "./providers";
+import { Analytics } from "@vercel/analytics/react"
 
 const myFont = localFont({
   src: "./fonts/FacultyGlyphic-Regular.ttf",
@@ -25,6 +26,7 @@ export default function RootLayout({
         <Providers>
           {children}
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
